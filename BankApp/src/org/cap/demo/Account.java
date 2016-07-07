256d8af035baf2bc16cbb84e47268853b019c18d
DivyaShanmugam <divya.shanmugam@capgemini.com>
2016-07-07 14:54:58 +0530

    Initial Commit

---
+++ BankApp/src/org/cap/demo/Account.java
@@ -0,0 +1,8 @@
+package org.cap.demo;
+
+public class Account {
+
+	public void greetings(){
+		System.out.println("Welcome All");
+	}
+}
---
+++ BankApp/src/org/cap/demo/MainClass.java
@@ -0,0 +1,16 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Account account=new Account();
+		account.greetings();
+		
+			System.out.println("Your bank operations");
+			System.out.println("Welcome");
+			System.out.println("Test1");
+		
+	}
+
+}
